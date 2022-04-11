f01cdd81f534b3be0b29b00d417abd531e3929b8
leonardo q <leonardoq.leao@outlook.com>
2022-04-11 11:06:44 -0300

    chore: removendo os  arquivos do react-creat-app que eu não vou precisar

---
+++ home/src/components/home/Home.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './css/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <details>
+          <code>oi</code>
+        </details>
+      </div>
+    )
+  }
+}
+
+export default Home;
