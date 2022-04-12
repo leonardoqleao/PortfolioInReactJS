7e16cd570c27cc92bf53cf380bb8f7e81024e60a
leonardo q <leonardoq.leao@outlook.com>
2022-04-11 23:22:51 -0300

    feat: alteração no ambiente para melhor visualização

--- home/src/template/home/Home.js
+++ home/src/template/home/Home.js
@@ -2,9 +2,14 @@ import { Component } from 'react';
 import './css/Home.css';
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+  }
+  state = {}
   render() {
     return (
-      <div className="App" >
+      <div className="Home">
+        asd
         <details>
           <code>oi</code>
         </details>
