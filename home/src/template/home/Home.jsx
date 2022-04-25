f14773c395f92bd6d312c679aa7693c7ba09a510
leonardo q <leonardoq.leao@outlook.com>
2022-04-25 12:32:44 -0300

    refactor: move constructor to Navbar file

--- home/src/template/home/Home.jsx
+++ home/src/template/home/Home.jsx
@@ -3,30 +3,10 @@ import Navbar from '../../components/navbar/Navbar';
 import './css/Home.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mobileResponsive: {
-        icon: {
-          activeIcon: 'navbarIconActive',
-          icon: 'icon'
-        },
-        options: 'navbar-options',
-        navbar: 'navbar'
-      }
-    }
-  }
-  onClickmobileResponsive = () => {
-    if (this.state.mobileResponsive.icon.activeIcon === 'navbarIconActive') {
-      this.setState({ mobileResponsive: { icon: { icon: 'mobile-icon', activeIcon: '' }, options: 'mobile-navbar-options', navbar: 'mobile-navbar' } })
-    } else {
-      this.setState({ mobileResponsive: { icon: { icon: 'icon', activeIcon: 'navbarIconActive' }, options: 'navbar-options', navbar: 'navbar' } })
-    }
-  }
   render() {
     return (
       <div className="Home">
-        <Navbar mobileResponsive={this.state.mobileResponsive} onClickmobileResponsive={this.onClickmobileResponsive} />
+        <Navbar/>
       </div>
     )
   }
