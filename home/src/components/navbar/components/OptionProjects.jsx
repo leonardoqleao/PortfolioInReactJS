de73b7b84e39f2c59a800bcb82e65bd69136503b
leonardo q <leonardoq.leao@outlook.com>
2022-04-25 09:41:24 -0300

    refactor: remove submenu projectos

--- home/src/components/navbar/components/OptionProjects.jsx
+++ home/src/components/navbar/components/OptionProjects.jsx
@@ -5,14 +5,6 @@ export default class OptionProjects extends Component {
         return (
             <li id='animationInProjectsOption' className='navbar-options'>
                 <a href="#projects">Projects</a>
-                <ul className='navbarUl2'>
-                    <li className='navbar-options'>
-                        <a href="#project-1">Project-1</a>
-                    </li>
-                    <li className='navbar-options'>
-                        <a href="#project-2">Project-2</a>
-                    </li>
-                </ul>
             </li>
         )
     }
