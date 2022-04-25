9cc85d0b7867ffaac9d27c7e9c3526d6dafa39ed
leonardo q <leonardoq.leao@outlook.com>
2022-04-25 03:54:39 -0300

    chore: update dependecies

--- home/src/index.js
+++ home/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import Home from './template/home/Home.js';
+import Home from './template/home/Home.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
