c3e2d8f915c691f3e04bad5522e56a31c4f8f35c
leonardo q <leonardoq.leao@outlook.com>
2022-04-28 12:57:05 -0300

    feat: update in meteorRaint func, set limit in top, left ramdon

--- home/src/components/home/components/func/meteorRain.js
+++ home/src/components/home/components/func/meteorRain.js
@@ -1,18 +1,36 @@
 export const rainMeteorFactory = (e) => {
-    function internalfun (left,top){
+    function internalfun(left, top, className) {
         const obj = {
-            top:top,
-            left:left
+            left: left,
+            top: top,
+            class: className
         }
         return obj
     }
     const newArray = []
+    const classArray = [
+        "rainchild-1",
+        "rainchild-2",
+        "rainchild-3",
+        "rainchild-4",
+        "rainchild-5",
+        "rainchild-6",
+        "rainchild-7",
+        "rainchild-8",
+        "rainchild-9",
+        "rainchild-10",
+        "rainchild-11",
+        "rainchild-12",  
+    ]
+    let j = 0
     for (let i = 0; i < e; i++) {
-        const top = -((Math.random() * 3000))
-        const left = ((Math.random() * 2000)+ 40)
-        const all = internalfun(left, top)
-        newArray[i] = all
         
+        if (j === 12) {j = 0}
+        const top = -((Math.random() * (1500)))
+        const left = ((Math.random() * (1000) + 40))
+        const all = internalfun(left, top, classArray[j])
+        newArray[i] = all
+        j++
     }
     return newArray
 }
