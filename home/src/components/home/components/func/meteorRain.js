4ad7a46940c4e3488ff2351882d98b9780b61ce8
leonardo q <leonardoq.leao@outlook.com>
2022-04-29 11:16:48 -0300

    feat: remove className test to metorRain fun

--- home/src/components/home/components/func/meteorRain.js
+++ home/src/components/home/components/func/meteorRain.js
@@ -1,4 +1,4 @@
-export const rainMeteorFactory = (e) => {
+export const rainMeteorFactory = (e, classArray) => {
     function internalfun(left, top, className) {
         const obj = {
             left: left,
@@ -8,24 +8,10 @@ export const rainMeteorFactory = (e) => {
         return obj
     }
     const newArray = []
-    const classArray = [
-        "rainchild-1",
-        "rainchild-2",
-        "rainchild-3",
-        "rainchild-4",
-        "rainchild-5",
-        "rainchild-6",
-        "rainchild-7",
-        "rainchild-8",
-        "rainchild-9",
-        "rainchild-10",
-        "rainchild-11",
-        "rainchild-12",  
-    ]
     let j = 0
     for (let i = 0; i < e; i++) {
         
-        if (j === 12) {j = 0}
+        if (j === classArray.length) {j = 0}
         const top = -((Math.random() * (1500)))
         const left = ((Math.random() * (1000) + 40))
         const all = internalfun(left, top, classArray[j])
