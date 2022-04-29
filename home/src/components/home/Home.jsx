8019330ba81f92960e2761517b4ba399b67965d6
leonardo q <leonardoq.leao@outlook.com>
2022-04-28 23:41:13 -0300

    refactor: move all dependecies name apresentation to file especifc

--- home/src/components/home/Home.jsx
+++ home/src/components/home/Home.jsx
@@ -5,6 +5,7 @@ import './css/home.css';
 import './css/keyframe.css';
 import './css/name-animation.css';
 import './css/rain-animation.css';
+import ApresentationName from "./components/apresentationName.jsx";
 
 
 export default class Home extends Component {
@@ -18,7 +19,7 @@ export default class Home extends Component {
         }
     }
     metorAlt = (e) => {
-        if(e.target.value <= 500){
+        if (e.target.value <= 500) {
             this.setState({
                 numbers: e.target.value
             })
@@ -27,12 +28,11 @@ export default class Home extends Component {
                 this.setState({
                     rainMeteorNumber: test
                 })
-            })
+            }, 300)
         } else {
             alert(`Value: ${e.target.value}, is invalid for animation: max value is: 500`)
             e.target.value = 500
         }
-        
     }
     componentDidMount() {
         const test = rainMeteorFactory(this.state.numbers)
@@ -43,41 +43,11 @@ export default class Home extends Component {
             this.setState({ latter: 'test' })
         }, 3600)
     }
-
-    componentDidUpdate() {
-
-    }
-    mouseOver = (e) => {
-        e.target.className = this.state.latter
-        setTimeout(() => {
-            e.target.className = ''
-            this.setState({ time: 1500 })
-        }, this.state.time)
-    }
-
     render() {
 
         return (
             <div className="home">
-                <div className="name">
-                    <h1 className="blockName">
-                        <div onMouseOver={this.mouseOver} className='bom'>H</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>i</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>,</div>
-                        <div className="displayNone">-</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>I</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>'</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>m</div><br />
-                        <div onMouseOver={this.mouseOver} className='bom'>L</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>e</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>o</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>n</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>a</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>r</div>
-                        <div onMouseOver={this.mouseOver} className='bom'>d</div>
-                        <div onMouseOver={this.mouseOver} className='bom' onAnimationEnd={this.rainMeteor} >o</div><br />
-                    </h1>
-                </div>
+                <ApresentationName/>
                 <div id="rain" >
                     {this.state.rainMeteorNumber.map(n => (
                         <div className={n.class} style={{ left: n.left, top: n.top, display: 'inline-block' }} ></div>
---
+++ home/src/components/home/components/apresentationName.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+
+export default class ApresentationName extends Component {
+    mouseOver = (e) => {
+        e.target.className = this.state.latter
+        setTimeout(() => {
+            e.target.className = ''
+            this.setState({ time: 1500 })
+        }, this.state.time)
+    }
+    render() {
+        return (
+            <div className="name">
+                <h1 className="blockName">
+                    <div onMouseOver={this.mouseOver} className='bom'>H</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>i</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>,</div>
+                    <div className="displayNone">-</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>I</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>'</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>m</div><br />
+                    <div onMouseOver={this.mouseOver} className='bom'>L</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>e</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>o</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>n</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>a</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>r</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>d</div>
+                    <div onMouseOver={this.mouseOver} className='bom'>o</div><br />
+                </h1>
+            </div>
+        )
+    }
+}
