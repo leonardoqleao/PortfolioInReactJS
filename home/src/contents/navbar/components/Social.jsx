ef45c151963f11139b3359a55e3a1565205c75dd
leonardo q <leonardoq.leao@outlook.com>
2022-05-01 15:55:46 -0300

    feat: add links in socialMidiaIcon

--- home/src/contents/navbar/components/Social.jsx
+++ home/src/contents/navbar/components/Social.jsx
@@ -1,14 +1,22 @@
 import { Component } from "react";
 
-export default class Social extends Component{
-    render (){
-        const{ social, display } = this.props
+export default class Social extends Component {
+    render() {
+        const { social, display } = this.props
         return (
-            <div className={social.class} style={{display: display}}>
-                <img className="socialIcon" src={social.github} alt="github" />
-                <img className="socialIcon" src={social.linkedin} alt="linkdin" />
-                <img className="socialIcon" src={social.gmail} alt="gmail" />
-                <img className="socialIcon" src={social.telegram} alt="telegram" />
+            <div className={social.class} style={{ display: display }}>
+                <a href="https://github.com/leonardoqleao" target={'_blank'} rel="noreferrer">
+                    <img className="socialIcon" src={social.github} alt="github" />
+                </a>
+                <a href="https://www.linkedin.com/in/leonardoqleao" target={'_blank'} rel="noreferrer">
+                    <img className="socialIcon" src={social.linkedin} alt="linkdin" />
+                </a>
+                <a href="mailto:leonardoq.leao@gmail.com" target={'_blank'} rel="noreferrer">
+                    <img className="socialIcon" src={social.gmail}  title='leonardoq.leao@gmail.com' alt="gmail" />
+                </a>
+                <a href="https://t.me/LeonardoQ_Leao" target={'_blank'} rel="noreferrer">
+                    <img className="socialIcon" src={social.telegram} alt="telegram" />
+                </a>
             </div>
         )
     }
