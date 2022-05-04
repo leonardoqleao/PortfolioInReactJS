e9c9375b11d2a4aad347db80415a4996d2fade93
leonardo q <leonardoq.leao@outlook.com>
2022-05-04 16:13:15 -0300

    feat: add movelie responsive in profile

--- src/contents/profile/Profile.jsx
+++ src/contents/profile/Profile.jsx
@@ -12,10 +12,19 @@ export default class Profile extends Component {
                         <p id="y2012">
                             Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
+                        <p id="lefty2015">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
+                        </p>
                         <p id="y2017">
                             Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
+                        <p id="lefty2020">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
+                        </p>
                         <p id="y2021">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
+                        </p>
+                        <p id="lefty2022">
                             Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
                     </div>
