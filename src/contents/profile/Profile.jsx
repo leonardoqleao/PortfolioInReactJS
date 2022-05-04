f94f5d5af5dc8de06b0fa0506b6098d8c7003f82
leonardo q <leonardoq.leao@outlook.com>
2022-05-04 15:05:03 -0300

    feat: add animation rightside

--- src/contents/profile/Profile.jsx
+++ src/contents/profile/Profile.jsx
@@ -10,31 +10,25 @@ export default class Profile extends Component {
                 <div className="timeline">
                     <div className="leftSide">
                         <p id="y2012">
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
                         <p id="y2017">
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
                         <p id="y2021">
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
                     </div>
-                    <LineBetween/>
+                    <LineBetween />
                     <div className="rightSide">
-                    <p>
+                        <p id="y2015">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
-                        <p>
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
+                        <p id="y2020">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
-                        <p>
-                        </p>
-                        <p>
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
-                        </p>
-                        <p>
-                        </p>
-                        <p>
-                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad 
+                        <p id="y2022">
+                            Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Viva Forevis aptent taciti sociosqu ad
                         </p>
                     </div>
                 </div>
